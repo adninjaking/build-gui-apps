910d5eda46527cb816f2a83d3cc1b5139e4d7347
adninjaking <thetechoftech28@gmail.com>
2026-03-12 19:54:27 +0000

    Add files via upload

---
+++ code.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from gtts import gTTS
+import os
+
+window = Tk()
+frame1 =Frame(window,bg = "light pink",height = "150")
+frame1.pack(fill = X)
+frame2 = Frame(window,bg = "light green",height = "750")
+frame2.pack(fill = X)
+label = Label(frame1,text = "text to speech",font="bold,30",bg = "light pink")
+label.place(x = 180,y=70)
+entry = Entry(frame2,width = 45,bd = 4,font = 14)
+entry.place(x = 50,y = 52)
+entry.insert(0,"")
+
+def play():
+    language = "en"
+    myobj = gTTS(text = entry.get(),lang = language,slow = False,tld = "fr")
+    myobj.save("convert.wav")
+    os.system("convert.wav")
+
+
+btn = Button(frame2,text = "submit",width = "15",pady = 10,font = "bold,15",command  = play,bg= "yellow")
+btn.place(x= 250,y = 130)
+window.title("text to speech converter")
+window.geometry("650x550+350+200")
+window.mainloop()
